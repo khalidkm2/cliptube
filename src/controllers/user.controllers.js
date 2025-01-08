959b9baed4a192f14f260f4cf0834604929b3c7c
khalid <khalidmiya399@gmail.com>
2025-01-08 06:57:51 +0545

    auth middleware

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -66,13 +66,111 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 
+
+const generateRefresshAndAccessToken = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        if(!user){
+            throw new ApiError(401,"user not found")
+        }
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+
+        return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(500,"failed to genrate token")
+    }
+}
+
+const loginUser = asyncHandler(async(req,res) => {
+    const {username,email,password} = req.body
+    if(!username || !email){
+        throw new ApiError(401,"username or email is required",)
+        if(!password){
+            throw new ApiError(401,"password is required")
+        }
+    }
+    const user = await User.findOne({
+        $or:[{email},{password}]
+    })
+    if(!user){
+        throw new ApiError(400,"User is not registered")
+    }
+    if(!user.isPassword(password)){
+        throw new ApiError(402,"Password is incorrect")
+    }
+    const {accessToken,refreshToken} = await generateRefresshAndAccessToken(user._id)
+
+
+    user.refreshToken = refreshToken
+    const fieldsToExclude = ["password","refreshToken"]  
+    const filteredUser = Object.entries(user).filter(([key,value]) => !fieldsToExclude.includes(key))
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(201).cookie("refreshToken",refreshToken,options).cookie("accessToken",accessToken,options).json(
+        new ApiResponse(201,{user: filteredUser},"user logged in successfully")
+    )
+
+})
+
+const logoutUser = asyncHandler(async(req,res) => {
+        await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                $set:{
+                    refreshToken: ""
+                }
+            },{
+                new: true
+            }
+        )
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        return res
+        .status(200)
+        .clear
+})
+
+
+
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
 
 
 
+
+
+
+
+
+//login
 /*
+login
+-take data
+-validate
+-find
+-compare
+-generate token
+-send
+
+*/
+
+
+
+
+/*
+register
 -validation
 -check if user already exist
 -create user (using methods,and create token,)
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import  jwt from "jsonwebtoken"
+import {User} from "../models/user.model.js"
+
+
+const verifyJwt = asyncHandler(async(req,res,next) => {
+    try {
+        const token = req.cookies?.accessToken || req.headers("authorization")?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(400,"Token not found")
+        }
+        const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decoded._id)
+        if(!user){
+            throw new ApiError(401,"You are not authorized")
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401,error?.message || "Invalid access token")
+    }
+})
